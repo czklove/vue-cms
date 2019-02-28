75319900e9fee27bf34528f6f1d0a24575ec5c25
czklove <czklove@126.com>
2019-02-28 21:41:00 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,12 @@
+//入口js文件
+import Vue from 'vue'
+import app from './App.vue'
+import {Header} from 'mint-ui'
+import './lib/mui/css/mui.min.css'
+
+Vue.component(Header.name,Header)
+
+var vm=new Vue({
+  el:'#app',
+  render:c=>c(app)
+})
